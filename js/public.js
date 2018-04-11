d4765bd91d0405e778fa368ea2ca0cb5655acc31
SaSaChu <teresa7397@gmail.com>
2018-04-11 18:00:40 +0800

    Fix html code.

--- js/public.js
+++ js/public.js
@@ -12,4 +12,25 @@ $(function () {
       $('html, body').animate ({ scrollTop: 0 }, 'slow');
   });
 
+  // 漢堡變換 + menu滑出
+  $(document).ready(function() {
+    $('#nav-icon3').click(function(){
+      $(this).toggleClass('open');
+
+      $('#r_menu_boxs').toggleClass('s_menu');
+      $('#menu_back').toggleClass('s_menu_b');
+    });
+  });
+  $('#r_menu_boxs').each (function () {
+    var $that = $(this);
+    $that.find ('span').click (function () {
+      $(this).toggleClass ('s');
+    });
+  });
+
+
+   // 圖片縮放
+  $(".spic").imgLiquid ();
+
+
 });
