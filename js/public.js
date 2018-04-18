7f8c548543836119bb52ff05d99323101309e0b2
OA Wu <comdan66@gmail.com>
2018-04-18 23:39:10 +0800

    fix code

--- js/public.js
+++ js/public.js
@@ -42,13 +42,16 @@ $(function () {
   $(".work_boxs").each (function () {
     var $that = $(this);
 
-    var $bb = $that.find ('.bb').attr ('data-i', 2);
+    var $bb = $that.find ('.bb').attr ('data-i', 1);
     var l = $bb.find ('>*').length;
     
     $that.find ('>a').click (function () {
       var i = parseInt ($bb.attr ('data-i'), 10);
-      $bb.attr ('data-i', $(this).hasClass ('.icon-chevron-left') ? (--i <= 0 ? l : i) : (++i > l ? 1 : i));
+      $bb.attr ('data-i', $(this).hasClass ('icon-chevron-left') ? (--i <= 0 ? l : i) : (++i > l ? 1 : i));
     });
+    var f = $bb.find ('>*').first ().clone (true);
+    var t = $bb.find ('>*').last ().clone (true);
+    $bb.append (f).prepend (t);
   });
 
 
@@ -56,11 +59,13 @@ $(function () {
   // banner
   $('.index-banners').each (function () {
     var $that = $(this).attr ('data-i', 1);
+    
     $that.find ('.bg').imgLiquid ({ verticalAlign:'center' });
     var l = $that.find ('>div>*').length;
     $that.find ('>a').click (function () {
       var i = parseInt ($that.attr ('data-i'), 10);
       $that.attr ('data-i', $(this).hasClass ('left') ? (--i <= 0 ? l : i) : (++i > l ? 1 : i));
+      
     });
     setInterval (function () {
       $that.find ('>a.left').click ();
